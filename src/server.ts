f85aea1449151af035240de19900b1b6931e80fa
ElemarLeonel <elemarleonelbalduino@gmail.com>
2025-05-13 12:37:46 -0400

    feat: Implemented CORS for API

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import todoRoutes from "./routes/todos";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/todos", todoRoutes);
 
